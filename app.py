95b6848ed52e59dcf9fc5a71732d1ecdfab6ce55
isabelvonah <isabelvonah@gmail.com>
2021-12-02 11:48:42 +0100

    connect db to index

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, request, render_template
+import sqlite3 as sql
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # open sqlite connection
+    con = sql.connect("data/perrons.db")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM perronkante;")
+    perrons = cur.fetchall()
+
+    # close sqlite connection
+    con.commit()
+    con.close()
+    return render_template("index.html", perrons=perrons)
 
 @app.route("/kuchen")
 def kuchen():
